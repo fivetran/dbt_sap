3f5da61880f1a72ff56bdbe4a7c7c7f271ca3b0a
Avinash Kunnath <avinash.kunnath@fivetran.com>
2023-05-31 11:57:40 -0700

    Modify variable config

--- models/intermediate/int_sap__0fi_gl_10_sums.sql
+++ models/intermediate/int_sap__0fi_gl_10_sums.sql
@@ -198,13 +198,11 @@ final as (
     inner join t001 
         on faglflext.rbukrs = t001.bukrs
         and faglflext.rclnt = t001.mandt
-    {% if var('faglflext_rclnt_var',[]) and var('faglflext_ryear_var',[]) and var('faglflext_rbukrs_var',[]) and var('faglflext_racct_var',[]) and var('faglflext_rldnr_var',[]) and var('faglflext_prctr_var',[]) %}
+    {% if var('faglflext_rclnt_var',[]) and var('faglflext_ryear_var',[]) and var('faglflext_rbukrs_var',[]) and var('faglflext_rldnr_var',[]) %}
     where faglflext.rclnt =  {{ var('faglflext_rclnt_var') }}
     and faglflext.ryear = {{ var('faglflext_ryear_var') }}
-    and faglflext.rbukrs = {{ var('faglflext_rbukrs_var') }}
-    and faglflext.racct = {{ var('faglflext_racct_var') }}
-    and faglflext.rldnr =  {{ var('faglflext_rldnr_var') }}
-    and faglflext.prctr = {{ var('faglflext_prctr_var') }} 
+    and faglflext.rbukrs = {{ var('faglflext_rbukrs_var') }} 
+    and faglflext.rldnr =  {{ var('faglflext_rldnr_var') }} 
     {% endif %}
 )
 
