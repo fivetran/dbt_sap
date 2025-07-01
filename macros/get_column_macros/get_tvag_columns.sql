7618d70bbade705e4d72c70d51630f1fed0e5fc6
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2025-07-01 09:57:37 -0500

    Add explicit column lists for remaining lookup macros

--- macros/get_column_macros/get_tvag_columns.sql
+++ macros/get_column_macros/get_tvag_columns.sql
@@ -1,6 +1,19 @@
 {% macro get_tvag_columns() %}
 
-{% set columns = [] %}
+{% set columns = [
+    {"name": "_fivetran_deleted", "datatype": "boolean"},
+    {"name": "_fivetran_rowid", "datatype": dbt.type_numeric()},
+    {"name": "_fivetran_synced", "datatype": dbt.type_timestamp()},
+    {"name": "abgru", "datatype": dbt.type_string()},
+    {"name": "dragr", "datatype": dbt.type_string()},
+    {"name": "ep_off", "datatype": dbt.type_string()},
+    {"name": "fk_erl", "datatype": dbt.type_string()},
+    {"name": "fsh_pqr_spec", "datatype": dbt.type_string()},
+    {"name": "hvr_change_time", "datatype": dbt.type_string()},
+    {"name": "hvr_is_deleted", "datatype": dbt.type_string()},
+    {"name": "kowrr", "datatype": dbt.type_string()},
+    {"name": "mandt", "datatype": dbt.type_string()},
+] %}
 
 {{ return(columns) }}
 
--- macros/get_column_macros/get_tvagt_columns.sql
+++ macros/get_column_macros/get_tvagt_columns.sql
@@ -1,6 +1,14 @@
 {% macro get_tvagt_columns() %}
 
-{% set columns = [] %}
+{% set columns = [
+    {"name": "_fivetran_deleted", "datatype": "boolean"},
+    {"name": "_fivetran_rowid", "datatype": dbt.type_numeric()},
+    {"name": "_fivetran_synced", "datatype": dbt.type_timestamp()},
+    {"name": "abgru", "datatype": dbt.type_string()},
+    {"name": "bezei", "datatype": dbt.type_string()},
+    {"name": "mandt", "datatype": dbt.type_string()},
+    {"name": "spras", "datatype": dbt.type_string()},
+] %}
 
 {{ return(columns) }}
 
